037379ec8bc71e5a9527004955a578ab014c23f5
Daniel Knobloch <dknobloch17@gmail.com>
2025-01-04 01:50:40 -0600

    resizing

--- src/lib/service/SyncService.svelte.ts
+++ src/lib/service/SyncService.svelte.ts
@@ -2,6 +2,9 @@ import type MinifluxApi from '$lib/api/MinifluxApi';
 import { db, type Episode, type Podcast } from '$lib/db/db';
 import type { Feed } from '$lib/types/miniflux';
 
+const maxWidth = 300;
+const maxHeight = 300;
+
 export class SyncService {
 	status = $state<string>('');
 
@@ -18,21 +21,24 @@ export class SyncService {
 	}
 
 	private async syncFeeds(categoryIds: number[]): Promise<Feed[]> {
-		const feeds = (
-			await Promise.all(
-				categoryIds.map(async (categoryId) => {
-					const categoryFeeds = await this.api.fetchFeedsForCategory(categoryId);
-					return Promise.all(
-						categoryFeeds.map(async (feed) => ({
-							...feed,
-							icon: await this.api.fetchFeedIcon(feed.id)
-						}))
-					);
-				})
-			)
-		).flat();
+		const categoryFeeds = await Promise.all(
+			categoryIds.map((categoryId) => this.api.fetchFeedsForCategory(categoryId))
+		);
+		const feeds = categoryFeeds.flat();
 
-		const podcasts: Podcast[] = feeds.map((feed) => ({
+		const feedsWithIcons = await Promise.all(
+			feeds.map(async (feed) => {
+				const icon = await this.api.fetchFeedIcon(feed.id);
+				const resizedIcon = await this.resizeBase64Image(icon, maxWidth, maxHeight);
+
+				return {
+					...feed,
+					icon: resizedIcon
+				};
+			})
+		);
+
+		const podcasts: Podcast[] = feedsWithIcons.map((feed) => ({
 			id: feed.id,
 			title: feed.title,
 			newEpisodes: 0,
@@ -40,7 +46,7 @@ export class SyncService {
 		}));
 		await db.podcasts.bulkPut(podcasts);
 
-		return feeds;
+		return feedsWithIcons;
 	}
 
 	private async syncFeedEntries(feed: Feed): Promise<void> {
@@ -55,5 +61,40 @@ export class SyncService {
 			isDownloaded: false
 		}));
 		await db.episodes.bulkPut(episodes);
+	}
+
+	private async resizeBase64Image(
+		dataUrl: string,
+		maxWidth: number,
+		maxHeight: number
+	): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const img = new Image();
+
+			img.onload = function () {
+				let { width, height } = img;
+				if (width > maxWidth || height > maxHeight) {
+					const aspectRatio = width / height;
+					if (width > height) {
+						width = maxWidth;
+						height = Math.round(width / aspectRatio);
+					} else {
+						height = maxHeight;
+						width = Math.round(height * aspectRatio);
+					}
+				}
+
+				const canvas = document.createElement('canvas');
+				canvas.width = width;
+				canvas.height = height;
+				const ctx = canvas.getContext('2d');
+				ctx!.drawImage(img, 0, 0, width, height);
+
+				resolve(canvas.toDataURL('image/png'));
+			};
+
+			img.onerror = () => reject(new Error('Failed to load image'));
+			img.src = `data:${dataUrl}`;
+		});
 	}
 }
