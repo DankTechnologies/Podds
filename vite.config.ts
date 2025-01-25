40399c30cdee56ba6cda0d0710fd0d04bc91adc2
Daniel Knobloch <dknobloch17@gmail.com>
2025-01-24 23:23:50 -0600

    update all deps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import fs from 'fs';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		host: '0.0.0.0',
+		allowedHosts: true,
 		https: {
 			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
 			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
