dc4eb1a52f3ad4c41646c7529b5393503566a1b3
Daniel Knobloch <dknobloch17@gmail.com>
2025-02-18 20:01:28 -0600

    revert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,23 @@
 <script lang="ts">
 	import '../normalize.css';
 	import '../app.css';
+	import { goto } from '$app/navigation';
 	import Player from '$lib/components/Player.svelte';
 	import BottomNavBar from '$lib/components/BottomNavBar.svelte';
-	import { FeedService } from '$lib/service/FeedService';
-	import { db } from '$lib/stores/db.svelte';
 	import { onMount } from 'svelte';
+	import { FeedService } from '$lib/service/FeedService';
+
 	let feedService = new FeedService();
 
-	let isDbLoaded = $derived(
-		!db.feeds.isLoading() && !db.episodes.isLoading() && !db.settings.isLoading()
-	);
-	let initialized = $state(true);
-
 	onMount(() => {
-		if (isDbLoaded) {
-			initialized = true;
-			feedService.startPeriodicUpdates();
-		}
+		feedService.startPeriodicUpdates();
 	});
 
 	let { children } = $props();
 </script>
 
-{#if !initialized}
-	<div class="loading">Loading...</div>
-{:else}
-	<main>
-		{@render children()}
-		<Player />
-		<BottomNavBar />
-	</main>
-{/if}
-
-<style>
-	.loading {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		font-size: 2rem;
-	}
-</style>
+<main>
+	{@render children()}
+	<Player />
+	<BottomNavBar />
+</main>
